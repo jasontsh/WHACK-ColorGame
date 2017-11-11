97d3554eb3b0b8d427b8853015c1acf31ace01c4
Jason Tang <tangsh@sas.upenn.edu>
2017-11-10 21:30:01 -0500

    first commit, finished xml prelim

---
+++ app/src/main/java/com/whack/janson/colorgame/MainActivity.java
@@ -0,0 +1,21 @@
+package com.whack.janson.colorgame;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView questionCircle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        questionCircle = (ImageView) findViewById(R.id.question_circle);
+        questionCircle.getDrawable().setColorFilter(Color.RED, PorterDuff.Mode.DST_IN);
+    }
+}
